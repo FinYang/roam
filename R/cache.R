62ce65b24f3ce4ca881de3f589b34dc5bab3118d
Yangzhuoran Yang <yangyangzhuoran@gmail.com>
2026-01-13 15:38:48 +0100

    Format code

--- R/cache.R
+++ R/cache.R
@@ -1,14 +1,17 @@
 cache_path <- function(package, name) {
   file.path(
     rappdirs::user_data_dir("r-roam"),
-    package, name
+    package,
+    name
   )
 }
 
-cache_path_data <- function(package, name)
+cache_path_data <- function(package, name) {
   cache_path(package, paste0(name, ".RData"))
-cache_path_version <- function(package, name)
+}
+cache_path_version <- function(package, name) {
   cache_path(package, paste0(name, ".txt"))
+}
 
 roam_cache <- function(x, version, package, name) {
   save(x, file = cache_path_data(package, name))
@@ -18,7 +21,6 @@ roam_cache <- function(x, version, package, name) {
 roam_unlink <- function(package, name) {
   unlink(cache_path_data(package, name))
   unlink(cache_path_version(package, name))
-  cat(sprintf('Cache of data "%s" in package "%s" is deleted',
-              name, package))
+  cat(sprintf('Cache of data "%s" in package "%s" is deleted', name, package))
   return(invisible(NULL))
 }
--- R/roam.R
+++ R/roam.R
@@ -64,39 +64,53 @@ new_roam <- function(package, name, obtainer, ...) {
   force(obtainer)
   x <- NULL
   nonexist_msg <- sprintf(
-    'The roam data object "%s" in package %s does not exist locally', name, package)
+    'The roam data object "%s" in package %s does not exist locally',
+    name,
+    package
+  )
   structure(
     function(...) {
       # Skip on tests, never test.
       # testing if installed package can be loaded from final location
       # triggers evaluation of active bindings
-      if(!is.na(Sys.getenv("R_TESTS", unset = NA))) return(invisible(NULL))
+      if (!is.na(Sys.getenv("R_TESTS", unset = NA))) {
+        return(invisible(NULL))
+      }
 
       # check object exists in cache
       file <- paste0(name, ".RData")
       path <- cache_path(package, file)
-      if(roam_flag$delete) {
+      if (roam_flag$delete) {
         roam_unlink(package, name)
         return(invisible(NULL))
       }
-      if(!file.exists(path) || roam_flag$install) {
+      if (!file.exists(path) || roam_flag$install) {
         # Check if it is evaluated by Rstudio autocomplete or autohelp
         # If it is, skip evaluation
         if (
           length(scalls <- sys.calls()) > 1 &&
-          (identical(scalls[[1]][[1]], as.name(".rs.rpc.get_completions")) ||
-           identical(scalls[[1]][[1]], as.name(".rs.rpc.get_help")))
+            (identical(scalls[[1]][[1]], as.name(".rs.rpc.get_completions")) ||
+              identical(scalls[[1]][[1]], as.name(".rs.rpc.get_help")))
         ) {
           return(invisible(NULL))
         }
         # if not interactive session
         # Only download using function or option
-        if(!roam_flag$install && isFALSE(getOption("roam.autodownload", default = FALSE))){
-          if(!interactive()) {
-            stop(paste(nonexist_msg, "You can automatically download missing roam objects by setting the `options(roam.autodownload = TRUE)`", sep = "\n"))
+        if (
+          !roam_flag$install &&
+            isFALSE(getOption("roam.autodownload", default = FALSE))
+        ) {
+          if (!interactive()) {
+            stop(paste(
+              nonexist_msg,
+              "You can automatically download missing roam objects by setting the `options(roam.autodownload = TRUE)`",
+              sep = "\n"
+            ))
           } else {
             cat(nonexist_msg)
-            if(!utils::askYesNo("Would you like to download and cache it?")) return(invisible(NULL))
+            if (!utils::askYesNo("Would you like to download and cache it?")) {
+              return(invisible(NULL))
+            }
           }
         }
         # obtain object with obtainer()
@@ -109,7 +123,7 @@ new_roam <- function(package, name, obtainer, ...) {
         on.exit(roam_flag$version <- NA_character_, add = TRUE)
         cat("Data retrieved")
         roam_cache(x, version = roam_flag$version, package, name)
-      } else if(is.null(x)){
+      } else if (is.null(x)) {
         # load() and return object from cache
         load(path)
         x <<- x
@@ -117,10 +131,11 @@ new_roam <- function(package, name, obtainer, ...) {
       x
     },
     class = "roam_object",
-    package = package, name = name
+    package = package,
+    name = name
   )
 }
-dir_create <- function(x){
+dir_create <- function(x) {
   dir.exists(x) || dir.create(x, recursive = TRUE)
 }
 
@@ -134,7 +149,7 @@ roam_flag$version <- NA_character_
 #' using the package writer/roam object creator defined obtainer function
 #' @return \code{roam_update} returns the updated local cache of the roam active binding
 #' @export
-roam_update <- function(x){
+roam_update <- function(x) {
   roam_install(x)
 }
 
@@ -170,7 +185,7 @@ roam_set_version <- function(version = NA_character_) {
 roam_version <- function(package, name) {
   file <- paste0(name, ".txt")
   path <- cache_path(package, file)
-  if(!file.exists(path)) {
+  if (!file.exists(path)) {
     cat("Not installed.")
     version <- NA_character_
   } else {
@@ -183,7 +198,7 @@ roam_version <- function(package, name) {
 
 #' @describeIn roam Delete the local cache of the roam active binding
 #' @export
-roam_delete <- function(x){
+roam_delete <- function(x) {
   roam_flag$delete <- TRUE
   on.exit(roam_flag$delete <- FALSE)
   x
@@ -194,20 +209,31 @@ roam_delete <- function(x){
 #' @return All the other functions return invisible \code{NULL}.
 #' @export
 roam_activate <- function(x) {
-  if(!inherits(x, "roam_object")) stop("Input is not a roam_object. Did you try to activate the same object twice?")
+  if (!inherits(x, "roam_object")) {
+    stop(
+      "Input is not a roam_object. Did you try to activate the same object twice?"
+    )
+  }
   env <- environment(environment(x)$obtainer)
   name <- attr(x, "name")
   # unlockBinding(name, env)
-  if(exists(name, envir = env)) remove(list = name, envir = env)
+  if (exists(name, envir = env)) {
+    remove(list = name, envir = env)
+  }
   makeActiveBinding(name, x, env)
 }
 
 #' @describeIn roam Activate all the roam objects in the given package.
 #' Used in the [.onLoad] function of a package
 #' @export
-roam_activate_all <- function(package){
+roam_activate_all <- function(package) {
   pkg_namespace <- as.list(asNamespace(package))
-  roam_which <- vapply(pkg_namespace, inherits, what = "roam_object", logical(1L))
+  roam_which <- vapply(
+    pkg_namespace,
+    inherits,
+    what = "roam_object",
+    logical(1L)
+  )
   lapply(pkg_namespace[roam_which], roam_activate)
   return(invisible(NULL))
 }
