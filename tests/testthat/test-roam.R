426eab4afacf5890a1cd06176d7bfc5fcb0eefc8
Yangzhuoran Yang <yangyangzhuoran@gmail.com>
2026-03-22 02:59:42 +0100

    R CMD check a package using roam in test

---
+++ tests/testthat/test-roam.R
@@ -0,0 +1,43 @@
+# should have locally installed roam
+test_package <- function(quiet = FALSE) {
+  pkg_path <- tempfile(pattern = "roamtest")
+  on.exit(unlink(pkg_path, recursive = TRUE))
+  usethis::create_package(pkg_path, roxygen = FALSE, open = FALSE)
+  # usethis::create_package(pkg_path, roxygen = FALSE)
+
+  writeLines(
+    sprintf(
+      r"(
+bee <- new_roam(
+  "%s",
+  "bee",
+  function(version) {
+    "bee"
+  }
+)
+.onLoad <- function(libname, pkgname) {
+  roam_activate_all("%s")
+})",
+      basename(pkg_path),
+      basename(pkg_path)
+    ),
+    con = file.path(pkg_path, "R", "roamtest.R")
+  )
+
+  cat(
+    "Depends: roam\n",
+    file = file.path(pkg_path, "DESCRIPTION"),
+    append = TRUE
+  )
+
+  check_output <- devtools::check(pkg_path, quiet = quiet, error_on = "never")
+  check_output
+}
+
+test_that("R CMD check of package that uses roam", {
+  skip_on_cran()
+  check_output <- test_package(quiet = TRUE)
+  expect_length(check_output$errors, 0)
+  expect_length(check_output$warnings, 0)
+  expect_length(check_output$notes, 0)
+})
